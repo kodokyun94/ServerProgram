7d0bd86b16d2861d118a3b043c1f60733eb521a1
KODOKYUN <hwasungnj3@gmail.com>
2024-05-17 11:19:08 +0900

    240517

---
+++ SampleServer/src/main/java/com/busanit501/sampleserver/connectTest/todo/domain/TodoVO.java
@@ -0,0 +1,19 @@
+package com.busanit501.sampleserver.connectTest.todo.domain;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+//@RequiredArgsConstructor
+@AllArgsConstructor
+public class TodoVO {
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private boolean finished;
+}
--- SampleServer/src/test/java/com/busanit501/sampleserver/connectTest/ConnectDbTest.java
+++ SampleServer/src/test/java/com/busanit501/sampleserver/connectTest/ConnectDbTest.java
@@ -1,12 +1,42 @@
 package com.busanit501.sampleserver.connectTest;
 
+import com.busanit501.sampleserver.connectTest.todo.domain.TodoVO;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.LocalDate;
 
 public class ConnectDbTest {
+    @Test
+    public void testHikariCP() throws Exception {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("webuser");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource dataSource = new HikariDataSource(config);
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn);
+        conn.close();
+    }
+
+    @Test
+    public  void test2(){
+        TodoVO todo = TodoVO.builder()
+                .tno(100L)
+                .title("제목100")
+                .dueDate(LocalDate.now())
+                .build();
+    }
+
     @Test
     public void test() {
         int v1 = 100;
